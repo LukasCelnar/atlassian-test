667c3a3fe93301b89705187263987d5ce70340cd
Lukáš Celnar <58079645+LukasCelnar@users.noreply.github.com>
2023-02-27 23:48:53 +0100

    Add files via upload

--- atlassian-forge/jira-test/src/index.jsx
+++ atlassian-forge/jira-test/src/index.jsx
@@ -1,15 +1,29 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, IssuePanel, useState, useEffect, Text } from '@forge/ui';
+import api from "@forge/api";
 
 const App = () => {
-  return (
-    <Fragment>
-      <Text>Hello world!</Text>
-    </Fragment>
-  );
+  const [projects, setProjects] = useState([]);
+
+  async function fetchProjects() {
+    const response = await api.asApp().requestJira("/rest/api/3/project/search");
+    const data = await response.json();
+    console.log("OUTPUT:", data)
+    setProjects(data);
+  }
+
+useEffect(() => {
+  fetchProjects();
+}, []);
+
+return (
+  <Fragment>
+    {projects.length > 0 ? <Text>Hello World</Text> : <Text>Error! {JSON.stringify(projects)}</Text>}
+  </Fragment>
+);
 };
 
 export const run = render(
-  <IssuePanel>
-    <App />
-  </IssuePanel>
+<IssuePanel>
+<App />
+</IssuePanel>
 );
