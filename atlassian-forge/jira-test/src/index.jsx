283e46e9a7533ce0e79170b2da9a019dc21d4d7c
Lukáš Celnar <58079645+LukasCelnar@users.noreply.github.com>
2023-02-28 19:03:22 +0100

    Add files via upload

--- atlassian-forge/jira-test/src/index.jsx
+++ atlassian-forge/jira-test/src/index.jsx
@@ -1,29 +1,59 @@
-import ForgeUI, { render, Fragment, IssuePanel, useState, useEffect, Text } from '@forge/ui';
-import api from "@forge/api";
+import ForgeUI, { render, Fragment, IssuePanel, useState, useEffect, Text, Table, Row, Head, Cell } from '@forge/ui';
+import api, { route } from "@forge/api";
+// https://atlassian.design/components/spinner/examples
+// import Spinner from '@atlaskit/spinner';
+// Loader throws error <Spinner interactionName="load" />
 
 const App = () => {
   const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchProjects() {
-    const response = await api.asApp().requestJira("/rest/api/3/project/search");
+    const response = await api.asApp().requestJira(route`/rest/api/3/project/search`);
     const data = await response.json();
     console.log("OUTPUT:", data)
-    setProjects(data);
+    setProjects(data.values);
+    setIsLoading(false);
   }
 
-useEffect(() => {
-  fetchProjects();
-}, []);
+  useEffect(async () => {
+    await fetchProjects();
+  }, []);
 
-return (
-  <Fragment>
-    {projects.length > 0 ? <Text>Hello World</Text> : <Text>Error! {JSON.stringify(projects)}</Text>}
-  </Fragment>
-);
+  return (
+    <Fragment>
+      {
+        isLoading ? <Text>Loading...</Text> :
+        projects.length > 0 ?
+        <Table>
+          <Head>
+            <Cell>
+              <Text>Key</Text>
+            </Cell>
+            <Cell>
+              <Text>Name</Text>
+            </Cell>
+          </Head>
+          {
+            projects.map(project => {
+              return <Row>
+                  <Cell>
+                    <Text>{project.key}</Text>
+                  </Cell>
+                  <Cell>
+                    <Text>{project.name}</Text>
+                  </Cell>
+                </Row>
+              })}
+        </Table> :
+        <Text>No Projects Found</Text>
+      }
+    </Fragment>
+  );
 };
 
 export const run = render(
-<IssuePanel>
-<App />
-</IssuePanel>
+  <IssuePanel>
+    <App />
+  </IssuePanel>
 );
